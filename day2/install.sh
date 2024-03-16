baef62890516ddd6a8f8425258ae33f56ad17ac5
blrk <blradhakrishnan@gmail.com>
2024-03-16 11:34:32 +0530

    fixed sudo

--- day2/install.sh
+++ day2/install.sh
@@ -1,7 +1,7 @@
 yum install docker -y
 systemctl enable docker.service
 systemctl start docker.service
-sudo systemctl status docker.service
+systemctl status docker.service
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
